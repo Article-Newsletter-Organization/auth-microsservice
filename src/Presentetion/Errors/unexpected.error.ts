d76f5edbc817d4085622be6527375c1913e8e8c4
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-01-23 18:36:29 -0300

    fix: unexpected message error

--- src/Presentetion/Errors/unexpected.error.ts
+++ src/Presentetion/Errors/unexpected.error.ts
@@ -1,16 +1,16 @@
 import { AppError } from '../Protocols';
 
 export class UnexpectedError extends AppError {
-  constructor(attrs?: {
-    message?: string;
-    target?: string;
-    stack?: string;
-    issues?: AppError[];
-  }) {
+  constructor(
+    attrs: {
+      message?: string;
+      target?: string;
+      stack?: string;
+      issues?: AppError[];
+    } = {},
+  ) {
     super({
-      message:
-        attrs.message ??
-        'Ocorreu um erro inesperado.',
+      message: attrs.message ?? 'Ocorreu um erro inesperado.',
       name: 'UnexpectedError',
       target: attrs?.target,
       stack: attrs?.stack,
