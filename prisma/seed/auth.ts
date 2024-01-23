5f18b511e784191d5f7d3a24d38c00979c9fa94b
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-01-23 18:21:19 -0300

    feat: seed added

---
+++ prisma/seed/auth.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  if ((await prisma.user.count()) == 0) {
+    const admin = await prisma.user.upsert({
+      where: { email: 'teste@example.com' },
+      update: {},
+      create: {
+        email: 'teste@example.com',
+        username: 'teste',
+        name: 'Teste',
+        password: await bcrypt.hash(
+          'strongpassword',
+          parseInt(process.env['SALT']),
+        ),
+        role: 'ADMIN',
+      },
+    });
+    console.log({ admin });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
