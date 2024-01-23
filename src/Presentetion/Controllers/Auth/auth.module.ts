02bbd4fd47d903e0e928fbe369cd678673013b59
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-01-23 15:45:23 -0300

    feat: redis configuration

--- src/Presentetion/Controllers/Auth/auth.module.ts
+++ src/Presentetion/Controllers/Auth/auth.module.ts
@@ -3,9 +3,17 @@ import AuthController from './auth.controller';
 import AuthService from './auth.service';
 import { PrismaModule } from 'src/Infra/prisma';
 import { UserRepository } from 'src/Data/Repositories';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisOptions } from 'src/Infra/redis';
+import config from 'src/Configuration/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    CacheModule.registerAsync(RedisOptions),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
   exports: [AuthModule],
