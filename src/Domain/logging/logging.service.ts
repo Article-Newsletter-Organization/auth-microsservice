7fa289c5004e256cc732342cc3333dff1ee06a87
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-06-20 14:28:27 -0300

    refac: removing logmodel

--- src/Domain/logging/logging.service.ts
+++ src/Domain/logging/logging.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, LoggerService } from '@nestjs/common';
 import { winstonOptions } from 'src/Infra/winston';
-import { HttpException } from 'src/Presentetion/Protocols';
 import { createLogger } from 'winston';
-import { LogModel } from './log.model';
 
 @Injectable()
 export class AppLoggerService implements LoggerService {
@@ -10,12 +8,12 @@ export class AppLoggerService implements LoggerService {
 
   constructor() {}
 
-  log(message: string, name: string, extra: LogModel) {
-    this.logger.info(`[${name}]: ${message}`, { name, ...extra });
+  log(message: string, name: string) {
+    this.logger.info(`[${name}]: ${message}`, { name });
   }
 
-  error(message: string, name: string, extra: LogModel) {
-    this.logger.error(`[${name}]: ${message}`, { name, ...extra });
+  error(message: string, name: string) {
+    this.logger.error(`[${name}]: ${message}`, { name });
   }
 
   warn(message: string, name: string) {
